42651b79a8014b927f2b2da39e024ee8b4ada888
thong <vanthongf@gmail.com>
2015-08-19 06:55:22 +0700

    *fix

---
+++ Assets/Scripts/GizmosController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+
+
+public class GizmosController : MonoBehaviour 
+{
+	
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	void Start () {
+		
+		Debug.Log (" -------------------------Start-------------------- ");
+	}
+	void Update(){
+		
+		Debug.Log (" --------------------------------------------- ");
+	}
+
+	void OnMouseDown(){
+		Debug.Log (" -------------------------OnMouseDown-------------------- ");
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+	
+	void OnMouseDrag(){
+		Debug.Log (" --------------------OnMouseDrag------------------------- ");
+		Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+		transform.position = cursorPosition;
+	}
+	
+}
