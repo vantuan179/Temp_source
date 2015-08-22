b97501d057bebd6b2a71b2aa3f65775bac9a74ac
aduc <leanhduc32@gmail.com>
2015-08-22 15:34:42 +0700

    * fix bug

--- Assets/Dart/Script/DragObjects.cs
+++ Assets/Dart/Script/DragObjects.cs
@@ -5,13 +5,18 @@ public class DragObjects : MonoBehaviour {
 	private bool _mouseState;
 	public GameObject Target;
 	private Vector3 screenSpace;
-	private Vector3 oldPostion;
+	private Vector3 oldpositionTarget;
+	private Vector3 oldRotationTarget;
+	private Vector3 oldRotationSpline;
 	private Vector3 oldMouse;
 	private Vector3 mouseSpeed;
 	private Rigidbody resetRigibody;
 	public int speed = 5;
 	private float[] listRadius;
 	private SplineWalker splineWalker;
+	private Vector3 tmpV;
+
+	private Vector3 oldPostionSpline;
 	// Use this for initialization
 	void Start () {
 		string[] tubes = {
@@ -23,19 +28,26 @@ public class DragObjects : MonoBehaviour {
 			listRadius[i] = obj.GetComponent<SphereCollider>().radius*transform.localScale.x;
 		}
 		splineWalker = Target.GetComponent <SplineWalker> ();
+		splineWalker.listRadius = listRadius;
+		tmpV = Target.transform.position - splineWalker.spline.transform.position;
+		oldpositionTarget = Target.transform.position;
+		oldRotationTarget = Target.transform.localEulerAngles;
+		oldRotationSpline = splineWalker.spline.transform.localEulerAngles;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
-			if(oldPostion == null){
-				oldPostion = Target.transform.position;
-			}
+			Target.transform.position = oldpositionTarget;
+			Target.transform.localEulerAngles = oldRotationTarget;
+			splineWalker.spline.transform.localEulerAngles = oldRotationSpline;
 			_mouseState = true;
 			Target.SetActive(true);
 			oldMouse = Input.mousePosition;
 			screenSpace = Camera.main.WorldToScreenPoint (Target.transform.position);
 			Target.transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
+
 			/*Vector3 O = Camera.main.WorldToScreenPoint (new Vector3(0, 0, 0));
 			Vector3 v = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, O.z));
 			float d = Vector3.Distance(v, new Vector3(0, 0, 0));
@@ -51,6 +63,11 @@ public class DragObjects : MonoBehaviour {
 		if (Input.GetMouseButtonUp (0)) {
 			//mouseSpeed = oldMouse - Input.mousePosition;
 			//Target.transform.parent = null;
+			Vector2 v1 = new Vector2(0, 1);
+			Vector2 v2 = new Vector2(Input.mousePosition.x - oldMouse.x, Input.mousePosition.y - oldMouse.y);
+			float angle = Vector2.Angle(v1, v2);
+			//splineWalker.spline.transform.Rotate(new Vector3(0, 0, 1), angle);
+			splineWalker.spline.transform.position = Target.transform.position - tmpV;
 			_mouseState = false;
 			splineWalker.isThrowDart = true;
 		}
@@ -65,5 +82,7 @@ public class DragObjects : MonoBehaviour {
 			Target.transform.position = curPosition;
 		}
 	}
+
+
 }
 
--- Assets/Dart/Script/SplineWalker.cs
+++ Assets/Dart/Script/SplineWalker.cs
@@ -13,7 +13,11 @@ public class SplineWalker : MonoBehaviour {
 	private float progress;
 	private bool goingForward = true;
 	public bool isThrowDart = false;
-
+	public float[] listRadius;
+	private GameObject objectText;
+	void Start () {
+		objectText = GameObject.Find("ObjectText");
+	}
 	private void Update () {
 		if (isThrowDart) {
 				if (goingForward) {
@@ -49,6 +53,55 @@ public class SplineWalker : MonoBehaviour {
 
 	public void reSetSplineWalker(){
 		progress -= 1f;
+		transform.localPosition = new Vector3 (transform.localPosition.x, transform.localPosition.y, 0);
+		objectText.GetComponent<TextMesh> ().text = "Scores : " + getScores ();
+
+	}	
+	int[] arrScores = {6,13,4,18,1,20,5,12,9,14,11,8,16,7,19,3,17,2,15,10};
+	int getScores(){
+		int scores = 0;
+		Vector3 axisOx = new Vector3 (1, 0, 0);
+		Vector3 v = new Vector3 (transform.localPosition.x, transform.localPosition.y, 0);
+		float angle = Vector3.Angle (axisOx, v);
+		if (transform.localPosition.y < 0) {
+			angle = 360 - angle;
+		}
+		//Debug.Log (" ----------------------angle---------------- "+angle);
+		for (int i = 0; i < arrScores.Length; i++) {
+			//Debug.Log(i + " ---------------------------- "+((360/arrScores.Length*(i+1) - 360/(arrScores.Length*2))));
+			if(angle < (360/arrScores.Length*(i+1) - 360/(arrScores.Length*2))) {
+				scores = arrScores[i];
+				break;
+			}
+		}
+		//if(true) return scores;
+
+		float d = Vector3.Distance(transform.localPosition, new Vector3(0, 0, 0));
+		for(int i = 0; i < listRadius.Length; i++){
+			if(d < listRadius[i]){
+				//Debug.Log("iiiiiiiii --------------------------------------- "+i);
+				switch(i){
+				case 0:
+						return 50;	
+					break;
+				case 1:
+					return 25;	
+					break;
+				case 2:
+				case 4:
+					return scores;	
+					break;
+				case 3:
+					return scores*3;	
+					break;
+				case 5:
+					return scores*2;	
+					break;
+				}
+				break;
+			}
+		}
+		return 0;
 	}
 
 }
