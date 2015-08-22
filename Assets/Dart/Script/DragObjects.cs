9417ae7d5efb9ffaffb42687b6cd0e9afaa410a5
aduc <leanhduc32@gmail.com>
2015-08-22 11:01:56 +0700

    * fix dart

--- Assets/Dart/Script/DragObjects.cs
+++ Assets/Dart/Script/DragObjects.cs
@@ -11,6 +11,7 @@ public class DragObjects : MonoBehaviour {
 	private Rigidbody resetRigibody;
 	public int speed = 5;
 	private float[] listRadius;
+	private SplineWalker splineWalker;
 	// Use this for initialization
 	void Start () {
 		string[] tubes = {
@@ -21,6 +22,7 @@ public class DragObjects : MonoBehaviour {
 			GameObject obj = GameObject.Find(tubes[i]);
 			listRadius[i] = obj.GetComponent<SphereCollider>().radius*transform.localScale.x;
 		}
+		splineWalker = Target.GetComponent <SplineWalker> ();
 	}
 	
 	// Update is called once per frame
@@ -28,18 +30,13 @@ public class DragObjects : MonoBehaviour {
 		if (Input.GetMouseButtonDown (0)) {
 			if(oldPostion == null){
 				oldPostion = Target.transform.position;
-			} else {
-				Target.transform.position = oldPostion;	
-				Target.rigidbody.isKinematic = true;
 			}
-
-			Target.rigidbody.useGravity = false;
 			_mouseState = true;
 			Target.SetActive(true);
 			oldMouse = Input.mousePosition;
 			screenSpace = Camera.main.WorldToScreenPoint (Target.transform.position);
 			Target.transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
-			Vector3 O = Camera.main.WorldToScreenPoint (new Vector3(0, 0, 0));
+			/*Vector3 O = Camera.main.WorldToScreenPoint (new Vector3(0, 0, 0));
 			Vector3 v = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, O.z));
 			float d = Vector3.Distance(v, new Vector3(0, 0, 0));
 			Debug.Log("d --------------------------------------- "+d);
@@ -48,17 +45,14 @@ public class DragObjects : MonoBehaviour {
 					Debug.Log("AA --------------------------------------- "+i);
 					break;
 				}
-			}
+			}*/
 
 		}
 		if (Input.GetMouseButtonUp (0)) {
-			mouseSpeed = oldMouse - Input.mousePosition;
-			Target.rigidbody.useGravity = true;
-			Target.rigidbody.isKinematic = false;
-			Target.transform.parent = null;
-			Target.rigidbody.AddForce(transform.forward*1000);
-			//Target.SetActive(false);
+			//mouseSpeed = oldMouse - Input.mousePosition;
+			//Target.transform.parent = null;
 			_mouseState = false;
+			splineWalker.isThrowDart = true;
 		}
 		if (_mouseState) {
 			//keep track of the mouse position
