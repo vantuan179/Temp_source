4b9d9187952d05a3eb653eb0eb5d152adb05d89a
aduc <leanhduc32@gmail.com>
2015-08-26 01:31:26 +0700

    - ui

---
+++ Assets/Dart/Script/Pause.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	public GameObject pauseButton, pausePanel;
+
+	public void Start()
+	{
+		OnUnPause ();
+	}
+
+	public void OnPause()
+	{
+		pausePanel.SetActive (true);
+		//pauseButton.SetActive (false);
+		Time.timeScale = 0;
+	}
+
+	public void OnUnPause()
+	{
+		pausePanel.SetActive(false);
+		//pauseButton.SetActive(true);
+		Time.timeScale = 1;
+	}
+}
