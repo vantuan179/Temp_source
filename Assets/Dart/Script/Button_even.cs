037a026e805db328b672b955b9cf47ecf6a9e5ec
aduc <leanhduc32@gmail.com>
2015-08-29 22:58:19 +0700

    - Button_even

---
+++ Assets/Dart/Script/Button_even.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button_even : MonoBehaviour {
+	public GameObject bt_camera, bt_eye, bt_foot, bt_infor ;
+	public void OnCamera()
+	{
+
+	}
+	public void OnEye()
+	{
+		
+	}
+	public void OnFoot()
+	{
+		
+	}
+	public void OnInfor()
+	{
+		
+	}
+}
