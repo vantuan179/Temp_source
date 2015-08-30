dc18ff2c88c9f6bb910fa861f72d013343fd6f61
aduc <leanhduc32@gmail.com>
2015-08-30 09:07:55 +0700

    - bt

--- Assets/Dart/Script/Button_even.cs
+++ Assets/Dart/Script/Button_even.cs
@@ -2,21 +2,47 @@
 using System.Collections;
 
 public class Button_even : MonoBehaviour {
-	public GameObject bt_camera, bt_eye, bt_foot, bt_infor ;
+	public GameObject bt_camera, bt_eye, bt_foot, bt_infor, bt_foot1, bt_eye1;
 	public void OnCamera()
 	{
 
+		//bt_camera1.SetActive = true;
 	}
 	public void OnEye()
 	{
-		
+		bt_camera.SetActive(false);
+		bt_eye.SetActive(false);
+		bt_eye1.SetActive(true);
+		bt_foot.SetActive(false);
+		bt_foot1.SetActive(true);
 	}
 	public void OnFoot()
 	{
-		
+		bt_camera.SetActive(false);
+		bt_eye.SetActive(false);
+		bt_eye1.SetActive(true);
+		bt_foot.SetActive(false);
+		bt_foot1.SetActive(true);
 	}
 	public void OnInfor()
 	{
 		
 	}
+	public void OnEye1()
+	{
+		bt_camera.SetActive(true);
+		bt_eye.SetActive(true);
+		bt_eye1.SetActive(false);
+		bt_foot.SetActive(true);
+		bt_foot1.SetActive(false);
+	}
+	public void OnFoot1()
+	{
+		bt_camera.SetActive(true);
+		bt_eye.SetActive(true);
+		bt_eye1.SetActive(false);
+		bt_foot.SetActive(true);
+		bt_foot1.SetActive(false);
+	}
+
 }
