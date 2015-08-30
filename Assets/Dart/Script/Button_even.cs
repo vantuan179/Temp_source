f8636a824a1a715f07f0828aa6ed14e79692d66a
aduc <leanhduc32@gmail.com>
2015-08-30 09:25:19 +0700

    - bt2

--- Assets/Dart/Script/Button_even.cs
+++ Assets/Dart/Script/Button_even.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Button_even : MonoBehaviour {
-	public GameObject bt_camera, bt_eye, bt_foot, bt_infor ;
+	public GameObject bt_camera, bt_eye, bt_foot, bt_infor, bt_eye1, bt_foot1 ;
 	DragObjects objDrag;
 	void Start () {
 		objDrag = GameObject.Find ("ObjectBoard").GetComponent<DragObjects>();	
@@ -13,6 +13,12 @@ public class Button_even : MonoBehaviour {
 	}
 	public void OnEye()
 	{
+		bt_camera.SetActive(false);
+		bt_eye.SetActive(false);
+		bt_eye1.SetActive(true);
+		bt_foot.SetActive(false);
+		bt_foot1.SetActive(true);
+		
 		SplineWalker.reviewCamera = true;
 		if (objDrag.cameraMode == CameraMode.ReviewDarts) {
 			objDrag.cameraMode = CameraMode.ReviewBoard;
@@ -23,6 +29,12 @@ public class Button_even : MonoBehaviour {
 	}
 	public void OnFoot()
 	{
+		bt_camera.SetActive(false);
+		bt_eye.SetActive(false);
+		bt_eye1.SetActive(true);
+		bt_foot.SetActive(false);
+		bt_foot1.SetActive(true);
+
 		if (objDrag.cameraMode == CameraMode.ReviewDarts) {
 			objDrag.cameraMode = CameraMode.MoveBoard;
 		} else {
@@ -33,4 +45,17 @@ public class Button_even : MonoBehaviour {
 	{
 		
 	}
+	public void OnEye1()
+	{
+		bt_camera.SetActive(true);
+		bt_eye.SetActive(true);
+		bt_eye1.SetActive(false);
+		bt_foot.SetActive(true);
+		bt_foot1.SetActive(false);
+	}
+	public void OnFoot1()
+	{
+
+	}
+
 }
